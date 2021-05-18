18aee223123a046cb0c016d6a63ae1331c49f805
A. Timoniq <timonik.ch@gmail.com>
2021-05-18 21:03:40 +0300

    fix included to secluded conversion

--- src/util.rs
+++ src/util.rs
@@ -3,7 +3,7 @@ use crate::{Domain, Value};
 pub fn convert_to_secluded(value: Value<i32>) -> Option<i32> {
     Some(
         match value {
-            Value::Included(i) => i - 1,
+            Value::Included(i) => i + 1,
             Value::Secluded(i) => i,
             Value::Infinite => {
                 return None;
